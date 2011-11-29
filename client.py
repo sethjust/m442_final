0d65e8d902bc923190265c29ab129faa2af086a9
Seth Just <sethjust@sethjust.com>
2011-11-28 19:04:55 -0800

    added helper methods to client

--- client.py
+++ client.py
@@ -3,8 +3,22 @@
 import sys,socket
 import time
 
+def recv(s):
+  d = ""
+  while (1):
+    c = s.recv(1)
+    if ord(c) == 0:
+      break;
+    d = d+c
+  return d
+
+def send_msg(s, msg):
+  s.send(msg+"\0")
+  return recv(s)
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("cerberus.sethjust.com", 26411));
-print s.send("test\0")
-time.sleep(.5)
-print s.send("STOP\0")
+
+print send_msg(s, "test")
+
+print send_msg(s, "STOP")
