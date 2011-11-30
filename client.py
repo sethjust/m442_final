bd26f2ea5e1e98059e2bc195ac5bccf4becb60c7
Seth Just <sethjust@sethjust.com>
2011-11-29 21:34:52 -0800

    made client send random strings for add

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys,socket
-import time
+import random,string
 
 def recv(s):
   d = ""
@@ -16,14 +16,16 @@ def send_msg(s, msg):
   s.send(msg+"\0")
   return recv(s)
 
-HOST = "cerberus.sethjust.com"
+def rname(len=8):
+ return ''.join(random.choice(string.ascii_lowercase) for x in range(len))
+
+HOST = "localhost"
 PORT = int(sys.argv[1]) if len(sys.argv)>1 else 11111
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT));
 
-print send_msg(s, "ADD:seth")
-print send_msg(s, "ADD:seth")
-print send_msg(s, "ADD:seth")
+for i in range(10):
+  print send_msg(s, "ADD:"+rname())
 
 print send_msg(s, "STOP")
