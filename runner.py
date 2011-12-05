26fdb4b65b7817fa8f29d478bd6fe746cdfacad7
Seth Just <sethjust@sethjust.com>
2011-12-05 13:00:58 -0800

    got sandboxed code reading files

--- runner.py
+++ runner.py
@@ -2,30 +2,94 @@
 
 # Adapted from http://readevalprint.github.com/blog/python-sandbox-with-pypy-part2.html
 
-import sys, shutil, os, tempfile
+import sys, os
+SANDBOX_BIN = os.path.abspath("./runner/pypy_src/pypy/translator/goal/pypy-c")
+SANDBOX_DIR = os.path.abspath("./runner/pypy_src/")
+sys.path.insert(0, SANDBOX_DIR)
+
+# import pypy sandbox now that it's added to the path
+#from pypy.translator.sandbox import pypy_interact
+# I have to re-write pypy_interact to support getting virtual files from the cloud
+from pypy.translator.sandbox import autopath
+from pypy.translator.sandbox.sandlib import SimpleIOSandboxedProc
+from pypy.translator.sandbox.sandlib import VirtualizedSandboxedProc
+from pypy.translator.sandbox.vfs import Dir, RealDir, RealFile
+from pypy.tool.lib_pypy import LIB_ROOT
+
+import stat, tempfile#, shutil
 from cStringIO import StringIO
 
 TIMEOUT = 5
 
 tempfile.tempdir = '/tmp/'
 
-SANDBOX_BIN = os.path.abspath("./runner/pypy_src/pypy/translator/goal/pypy-c")
-SANDBOX_DIR = os.path.abspath("./runner/pypy_src/")
-sys.path.insert(0, SANDBOX_DIR)
+class VirtualizedRunnerProc(VirtualizedSandboxedProc):
+    def __init__(self, *args, **kwds):
+        super(VirtualizedRunnerProc, self).__init__(*args, **kwds)
 
-# import pypy now that it's added to the path
-from pypy.translator.sandbox import pypy_interact
+    def do_ll_os__ll_os_fstat(self, fd): #this call is missing from the other class, preventing us from using open
+      file = self.get_file(fd)
+      dirnode = self.virtual_root
+      components = [component for component in file.name.split('/')]
+      for component in components:
+          if component:
+              dirnode = dirnode.join(component)
+              if dirnode.kind != stat.S_IFDIR:
+                  raise OSError(errno.ENOTDIR, component)
+      return node.stat()
+
+class PyPySandboxedProc(VirtualizedRunnerProc, SimpleIOSandboxedProc):
+    debug = True
+    argv0 = '/bin/pypy-c'
+    virtual_cwd = '/tmp'
+    virtual_env = {}
+    virtual_console_isatty = True
+
+    def __init__(self, executable, arguments, tmpdir=None):
+        self.executable = executable = os.path.abspath(executable)
+        self.tmpdir = tmpdir
+        super(PyPySandboxedProc, self).__init__([self.argv0] + arguments,
+                                                executable=executable)
+
+    def build_virtual_root(self):
+        # build a virtual file system:
+        # * can access its own executable
+        # * can access the pure Python libraries
+        # * can access the temporary usession directory as /tmp
+        exclude = ['.pyc', '.pyo']
+        if self.tmpdir is None:
+            tmpdirnode = Dir({})
+        else:
+            tmpdirnode = RealDir(self.tmpdir, exclude=exclude)
+        libroot = str(LIB_ROOT)
+
+        return Dir({
+            'bin': Dir({
+                'pypy-c': RealFile(self.executable),
+                'lib-python': RealDir(os.path.join(libroot, 'lib-python'),
+                                      exclude=exclude), 
+                'lib_pypy': RealDir(os.path.join(libroot, 'lib_pypy'),
+                                      exclude=exclude),
+                }),
+             'tmp': tmpdirnode,
+             })
 
 def exec_sandbox(code):
     try:
         tmpdir = tempfile.mkdtemp()
-        tmpscript = open(os.path.join(tmpdir, "script.py"),'w') # script.py is the name of the code passed in
-        tmpscript.write(code)
-        tmpscript.close()
-        sandproc = pypy_interact.PyPySandboxedProc(
+
+        tmpfile = open(os.path.join(tmpdir, "test"),'w')
+        tmpfile.write("This file is actually " + tmpfile.name)
+        tmpfile.close()
+
+#        tmpscript = open(os.path.join(tmpdir, "script.py"), 'w')
+#        tmpscript.write(code)
+#        tmpscript.close()
+
+        sandproc = PyPySandboxedProc(
             SANDBOX_BIN,
-            ['-c', code], # un comment this and comment the next line if you want to run code directly
-#            ['/tmp/script.py','--timeout',str(TIMEOUT),],
+            ['-c', code,'--timeout',str(TIMEOUT),],
+            #['/tmp/script.py','--timeout',str(TIMEOUT),],
            tmpdir # this is dir we jsut made that will become /tmp in the sandbox
         )
         try:
@@ -49,13 +113,16 @@ if __name__ == "__main__":
   s = ComputeCloud("134.10.30.239", 11111)
 
   code = '''
-print 'Hi there!'
+import os
+print open("/tmp/test", 'r').read()
 '''
 
-  f = s.add_string(rname(), code)
-  print f
+#  f = s.add_string(rname(), code)
+#  print f
+#  out, err = exec_sandbox(f.get())
 
-  out, err = exec_sandbox(f.get())
+  out, err = exec_sandbox(code)
+
   print '=' *10
   print 'OUTPUT\n%s' % out
   print '=' *10
